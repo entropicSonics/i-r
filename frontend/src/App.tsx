2c72b9caeaa81815e2ed197132e3a32907b4abfb
Kent <17228477+Hum4n01d@users.noreply.github.com>
2024-03-23 23:15:05 -0700

    selection area

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,27 @@
 // import { useState } from 'react'
-import './App.css'
+import "./App.css";
 // import the Editor component
-import Editor from '@/components/ui/Editor.tsx'
-import { Button } from "@/components/ui/button"
+import Editor from "@/components/ui/Editor.tsx";
+import { Button } from "@/components/ui/button";
 // Import the Input component
-import { Input } from "@/components/ui/input"
+import { Input } from "@/components/ui/input";
 // Import the metatopic component
-import Metatopic from '@/components/ui/Metatopic.tsx'
+import Metatopic from "@/components/ui/Metatopic.tsx";
 
-import { Drawer } from 'vaul';
+import { Drawer } from "vaul";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className = "app-wrapper">
-        <div className = "editor-controls flex justify-end">
+      <div className="app-wrapper">
+        <div className="editor-controls flex justify-end">
           <Drawer.Root>
             <Drawer.Trigger>
-              <Button className = "rounded-full border-2 bg-gray-100 text-slate-900 hover:text-slate-200">Categorize</Button>
+              <Button className="rounded-full border-2 bg-gray-100 text-slate-900 hover:text-slate-200">
+                Categorize
+              </Button>
             </Drawer.Trigger>
             <Drawer.Portal>
               <Drawer.Overlay className="fixed inset-0 bg-black/40" />
@@ -31,13 +33,26 @@ function App() {
                     {/* Drawer Contents */}
                     <Input
                       className="mb-4"
-                      placeholder="Search for a database" />
-                    
-                    <div className = "metatopic-list">
-                      <Metatopic title="Private Equity Research" activity="Last Activity on Dec 2, 2023 at 11:34pm" />
-                      <Metatopic title="AI Art" activity="Last Activity Today at 1:34pm" />
-                      <Metatopic title="Design Investment Thesis" activity="Last Activity on Oct 12, 2023" />
-                      <Metatopic title="Vedic Philosophy" activity="Last Activity at 2:04pm" />
+                      placeholder="Search for a database"
+                    />
+
+                    <div className="metatopic-list">
+                      <Metatopic
+                        title="Private Equity Research"
+                        activity="Last Activity on Dec 2, 2023 at 11:34pm"
+                      />
+                      <Metatopic
+                        title="AI Art"
+                        activity="Last Activity Today at 1:34pm"
+                      />
+                      <Metatopic
+                        title="Design Investment Thesis"
+                        activity="Last Activity on Oct 12, 2023"
+                      />
+                      <Metatopic
+                        title="Vedic Philosophy"
+                        activity="Last Activity at 2:04pm"
+                      />
                     </div>
                   </div>
                 </div>
@@ -45,13 +60,12 @@ function App() {
             </Drawer.Portal>
           </Drawer.Root>
         </div>
-        <div className = "editor-wrapper">
+        <div className="editor-wrapper">
           <Editor />
         </div>
-
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
