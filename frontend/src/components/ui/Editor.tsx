456a819ed71887c18860e0f5870793dd94d37ed9
Kent <17228477+Hum4n01d@users.noreply.github.com>
2024-03-23 23:12:52 -0700

    fix selecting placeholder

--- frontend/src/components/ui/Editor.tsx
+++ frontend/src/components/ui/Editor.tsx
@@ -1,37 +1,45 @@
-import React from 'react';
+import React from "react";
 
 // import {AutoFocusPlugin} from '@lexical/react/LexicalAutoFocusPlugin';
-import {LexicalComposer} from '@lexical/react/LexicalComposer';
-import {PlainTextPlugin} from '@lexical/react/LexicalPlainTextPlugin';
-import {ContentEditable} from '@lexical/react/LexicalContentEditable';
-import {HistoryPlugin} from '@lexical/react/LexicalHistoryPlugin';
-import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+import { LexicalComposer } from "@lexical/react/LexicalComposer";
+import { PlainTextPlugin } from "@lexical/react/LexicalPlainTextPlugin";
+import { ContentEditable } from "@lexical/react/LexicalContentEditable";
+import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
+import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
 
 interface Props {}
 
-const theme = {}
+const theme = {};
 
-function onError(error: Error):void {
-    console.error(error);
+function onError(error: Error): void {
+  console.error(error);
 }
 
 const Editor: React.FC<Props> = () => {
-    // Component logic goes here
-    const initialConfig = {
-        namespace: 'editor',
-        theme,
-        onError,
-    }
+  // Component logic goes here
+  const initialConfig = {
+    namespace: "editor",
+    theme,
+    onError,
+  };
 
-    return (
-        <LexicalComposer initialConfig={initialConfig}>
-                <PlainTextPlugin 
-                    contentEditable={<ContentEditable className='editor' />}
-                    placeholder = {<div className="editor-placeholder text-gray-400">Start typing...</div>}
-                    ErrorBoundary={LexicalErrorBoundary}/>                
-                <HistoryPlugin />
-        </LexicalComposer>
-    );
+  return (
+    <LexicalComposer initialConfig={initialConfig}>
+      <PlainTextPlugin
+        contentEditable={<ContentEditable className="editor" />}
+        placeholder={
+          <div
+            className="editor-placeholder text-gray-400"
+            style={{ pointerEvents: "none" }}
+          >
+            Start typing...
+          </div>
+        }
+        ErrorBoundary={LexicalErrorBoundary}
+      />
+      <HistoryPlugin />
+    </LexicalComposer>
+  );
 };
 
 export default Editor;
